e7f672a4571c2425e801091c6e3b765a448d678f
RyooChan <fbcks97@naver.com>
2022-01-05 23:05:14 +0900

    이미지클릭제거JS구현

--- src/main/resources/static/bootstrap-5.1.3-dist/js/form.js
+++ src/main/resources/static/bootstrap-5.1.3-dist/js/form.js
@@ -1,4 +1,3 @@
-
 function setThumbnail(target){
     for(var image of target.files){
         let reader = new FileReader();
@@ -12,6 +11,23 @@ function setThumbnail(target){
     }
 }
 
-document.getElementById("picture").addEventListener(
-    "change", event=>setThumbnail(event.target)
-);
+function removeThumbnail(target){
+    if(document.querySelector("#view_area").childElementCount <= 0) return;     // 미리보기 삭제할 요소 없으면 return
+    document.querySelector("#view_area").removeChild(target);       // 미리보기 이미지 삭제
+    target.remove();    // 파일 삭제
+}
+
+function init(){
+    // 다중 파일 미리보기 함수 적용
+    document.getElementById("picture").addEventListener(
+        "change", event=>setThumbnail(event.target)
+    );
+
+    // 파일 클릭시 삭제하기
+    document.getElementById("view_area").addEventListener(
+        "click", event=>removeThumbnail(event.target)
+    )
+}
+
+
+init();
