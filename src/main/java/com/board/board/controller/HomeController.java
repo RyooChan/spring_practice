9d46262d4a771d05acdd1176de5e4ca756345216
RyooChan <fbcks97@naver.com>
2022-01-29 18:39:13 +0900

    로그인상태부여
    httpSession을 통한 로그인 상태 프로필사진, 아이디, 권한 display

--- src/main/java/com/board/board/controller/HomeController.java
+++ src/main/java/com/board/board/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.board.board.controller;
 
+import com.board.board.dto.oauth.OAuthAttributes;
+import com.board.board.dto.oauth.SessionUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,6 +14,5 @@ public class HomeController {
     public String home(){
         return "home";
     }
-
 
 }
